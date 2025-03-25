1db003125fa6856b91b4f0d7a7f4aa8f4fa3b965
joonatanehlvest <joonatanehlvest123@gmail.com>
2025-03-25 16:07:40 +0200

    added prettier config for consistent formatting

--- fem-viewer/packages/parser/src/index.ts
+++ fem-viewer/packages/parser/src/index.ts
@@ -1,7 +1,12 @@
 import { XMLParser, XMLBuilder } from "fast-xml-parser";
 import { addXMLAttrPrefix, ATTR_PREFIX } from "./utils";
 import { Connector } from "@fem-viewer/types";
-import { Instance, ColorPicker, INSTANCE_DEFAULTS, BorderColorPicker } from "@fem-viewer/types/Instance";
+import {
+	Instance,
+	ColorPicker,
+	INSTANCE_DEFAULTS,
+	BorderColorPicker,
+} from "@fem-viewer/types/Instance";
 import { InstanceClass } from "@fem-viewer/types";
 import { Model } from "@fem-viewer/types";
 import { ModelAttributes } from "@fem-viewer/types";
@@ -9,13 +14,10 @@ import { XMLObj } from "./types";
 import { Interrefs } from "@fem-viewer/types/Instance";
 
 // Import from baseParser.ts
-import { parseXMLToModel, buildXMLFromParsed } from './baseParser';
-
-// Import from doctypeParser.ts
-import { parseXMLToModelPreserveDoctype, buildXMLFromParsedPreserveDoctype } from './doctypeParser';
+import { parseXMLToModel } from "./baseParser";
 
 // Import from editor.ts
-import { XMLEditor, createXMLEditor } from './editor';
+import { XMLEditor, createXMLEditor } from "./editor";
 
 class Parser {
 	_parsedXML: any;
@@ -425,7 +427,6 @@ const createParser = (XMLData: any): Parser => {
 	return new Parser(jObj);
 };
 
-
 export default createParser;
 
 export { Parser };
@@ -434,9 +435,11 @@ export { Parser };
 export { addXMLAttrPrefix, ATTR_PREFIX };
 
 // Re export baseParser and doctypeParser
-export { parseXMLToModel, buildXMLFromParsed } from './baseParser';
-export { parseXMLToModelPreserveDoctype, buildXMLFromParsedPreserveDoctype } from './doctypeParser';
+export { parseXMLToModel, buildXMLFromParsed } from "./baseParser";
+export {
+	parseXMLToModelPreserveDoctype,
+	buildXMLFromParsedPreserveDoctype,
+} from "./doctypeParser";
 
 // Export XMLEditor
 export { XMLEditor, createXMLEditor };
-
